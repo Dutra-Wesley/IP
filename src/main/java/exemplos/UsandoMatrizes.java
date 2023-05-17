6b6c9a63315c375310cf8494b17891902049f766
Wesley Dutra <Wesley-Dutra17@hotmail.com>
2023-05-17 02:30:36 -0300

    example about arrays

---
+++ src/main/java/exemplos/UsandoMatrizes.java
@@ -0,0 +1,55 @@
+package exemplos;
+
+public class UsandoMatrizes {
+
+	public static void main(String[] args) {
+
+		int[] numeros = { 3, 7, 1, 4 };
+
+		System.out.println(numeros);
+
+		System.out.println(numeros[2]);
+
+		String[] meias = { "Meia Branca", "Meia Vermelha", "Meia Azul" };
+
+		System.out.println(meias);
+
+		System.out.println(meias[0]);
+
+		for (int i = 0; i < meias.length; i++) {
+
+			System.out.println(meias[i]);
+
+		}
+
+		System.out.println("\n----------------\n");
+
+		int[][] matriz = { { 1, 3, 5 }, { 2, 4, 6 } };
+
+		System.out.println(matriz);
+
+		System.out.println("\n----------------\n");
+
+		for (int i = 0; i < matriz.length; i++) {
+
+			System.out.println(matriz[i]);
+
+		}
+
+		System.out.println("\n----------------\n");
+
+		for (int i = 0; i < matriz.length; i++) {
+
+			for (int j = 0; j < matriz[i].length; j++) {
+
+				System.out.print(matriz[i][j] + " ");
+
+			}
+
+			System.out.println();
+
+		}
+
+	}
+
+}
